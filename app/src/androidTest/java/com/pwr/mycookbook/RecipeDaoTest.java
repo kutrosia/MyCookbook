247375f77f66532be6d275f9b4ff2cea6bd2cb7b
Ola <olakutrowska@gmail.com>
2018-01-01 18:38:36 +0100

    Added user firebase authentication, created firebase database

--- app/src/androidTest/java/com/pwr/mycookbook/RecipeDaoTest.java
+++ app/src/androidTest/java/com/pwr/mycookbook/RecipeDaoTest.java
@@ -5,11 +5,6 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.pwr.mycookbook.model.dao.CategoryDao;
-import com.pwr.mycookbook.model.dao.RecipeDao;
-import com.pwr.mycookbook.model.repository.AppDatabase;
-import com.pwr.mycookbook.model.model.Recipe;
-
 import org.junit.*;
 import org.junit.runner.RunWith;
 
